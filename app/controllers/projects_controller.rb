3fdd25a1d9bf774e8352db473e04bac010352e10
James Robinson <james.michael.robinson@googlemail.com>
2014-10-18 01:55:47 -0700

    Add create project functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,19 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    current_user.projects.create(project_params)
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:project_id)
+  end
 end
