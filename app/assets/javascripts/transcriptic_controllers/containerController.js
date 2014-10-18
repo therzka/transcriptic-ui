e882b3c5b249f8133e29883322e11ac623bc71e2
James Robinson <james.michael.robinson@googlemail.com>
2014-10-18 16:52:30 -0700

    Setup container creation form (no saving)

--- app/assets/javascripts/transcriptic_controllers/containerController.js
+++ app/assets/javascripts/transcriptic_controllers/containerController.js
@@ -3,4 +3,8 @@ Transcriptic.ContainerController = function(containerForm) {
   this.containerForm.bindEventListeners(this);
 };
 
-Transcriptic.ContainerController.prototype = {};
+Transcriptic.ContainerController.prototype = {
+  createContainer: function(containerName, containerID, containerType, storage) {
+    console.log(containerName);
+  }
+};
--- app/assets/javascripts/transcriptic_views/containerForm.js
+++ app/assets/javascripts/transcriptic_views/containerForm.js
@@ -8,8 +8,19 @@ Transcriptic.ContainerForm.prototype = {
     this.controller = controller;
 
     this.$showForm.on("click", this.showNewContainerForm.bind(this));
+    this.$containerForm.on("submit", this.handleContainerFormSubmit.bind(this));
   },
   showNewContainerForm: function(evt) {
-   
+    this.$containerForm.show();
+  },
+  handleContainerFormSubmit: function(evt) {
+    evt.preventDefault();
+    
+    var containerName = this.$containerForm.find("#container_name").val();
+    var containerID = this.$containerForm.find("#container_name").val();
+    var containerType = this.$containerForm.find("#container_name").val();
+    var storage = this.$containerForm.find("#container_name").val();
+
+    this.controller.createContainer(containerName, containerID, containerType, storage)
   }
 };
