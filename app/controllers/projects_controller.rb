7fb0910dab10b71ba00b6194fda1c0017965ddec
James Robinson <james.michael.robinson@googlemail.com>
2014-10-18 01:43:15 -0700

    Add show project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,10 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+    @projects = current_user.projects.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :projects
+  
   root "projects#index"
 end
