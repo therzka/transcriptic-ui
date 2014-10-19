3c96def7a6982d9a479db03884185accad11e1fc
James Robinson <james.michael.robinson@googlemail.com>
2014-10-19 15:51:39 -0700

    Refactor request maker to Transcriptic.API

--- app/assets/javascripts/transcriptic_models/run.js
+++ app/assets/javascripts/transcriptic_models/run.js
@@ -7,71 +7,71 @@ Transcriptic.Run.prototype = {
   addInstruction: function(instruction) {
     this.instructions.push(instruction);
   },
-  encodeInstructions: function(){
-    var finalObj = {instructions:[]};
+  encodeInstructions: function() {
+    var instructions = [];
 
-    for (var i in this.instructions){
+    for (var i in this.instructions) {
       var ins  = this.instructions[i];
       var type = ins.instructionType;
-      var instructionList = []
+      var instructionList = [];
 
       if (type){
         instructionList.push({
-                op: type,
-                groups:[]
+          op: type,
+          groups:[]
         });
-      for (var x in instructionList){
-            if (instructionList[x]["op"] == type){
-              instructionList[x]["groups"].push(ins.encodeAction());
-            }
-            else{
-              instructionList.push({
-                op: type,
-                groups:[]
-              });
-            }   
+        for (var x in instructionList) {
+          if (instructionList[x]["op"] === type) {
+            instructionList[x]["groups"].push(ins.encodeAction());
+          }
+          else{
+            instructionList.push({
+              op: type,
+              groups:[]
+            });
+          }   
         }
       }
       else{
-        finalObj["instructions"].push(ins.encodeAction());
+        instructions.push(ins.encodeAction());
       }
-        
-      for (var instruction in instructionList){
-        finalObj["instructions"].push(instructionList[instruction])
+
+      for (var instruction in instructionList) {
+        instructions.push(instructionList[instruction]);
       }
     }
-    return JSON.stringify(finalObj, null, '\t')
+    return instructions;
   },
   encodeRefs: function(){
-    var finalObj = {refs:{}};
-    var containers = []
+    var refs = {};
+    var containers = [];
 
-    for (var ins in this.instructions){
-      if (containers.indexOf(this.instructions[ins].container) == -1){
+    for (var ins in this.instructions) {
+      if (containers.indexOf(this.instructions[ins].container) === -1) {
         containers.push(this.instructions[ins].container)
       }
     }
-    for (var c in containers){
-      if (containers[c].containerID == "new"){
-        finalObj["refs"][containers[c].containerName] = 
-          {
-            new: containers[c].containerType,
-            store: {
-              where: containers[c].storage
-            }
+    for (var c in containers) {
+      if (containers[c].containerID === "new") {
+        refs[containers[c].containerName] = 
+        {
+          new: containers[c].containerType,
+          store: {
+            where: containers[c].storage
           }
+        }
       }
       else{
-        finalObj["refs"][containers[c].containerName] = 
-          {
+        refs[containers[c].containerName] = 
+        {
           id: containers[c].containerID,
           store: {
-              where: containers[c].storage
+            where: containers[c].storage
           }
         }
       }
     }
-    return JSON.stringify(finalObj, null, '\t')
-    }
+    return refs;
+  }
   
 };
