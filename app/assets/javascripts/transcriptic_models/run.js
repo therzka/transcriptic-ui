533782d88d9f18495f6c7df905fe829b258362d6
Tali Herzka <tali@herzka.com>
2014-10-18 18:49:36 -0700

    beginning of encoding instructions

--- app/assets/javascripts/transcriptic_models/run.js
+++ app/assets/javascripts/transcriptic_models/run.js
@@ -7,4 +7,32 @@ Transcriptic.Run.prototype = {
   addInstruction: function(instruction) {
     this.instructions.push(instruction);
   }
+  encodeInstructions: function(){
+    finalObj = {instructions:[]};
+
+    for (var i in this.instructions){
+      containername = this.instructions[c].containerName;
+      containertype = this.instructions[c].containerType;
+      storage = this.instructions[c].storage;
+      containerid = this.instructions[c].containerID;
+
+      if (containerid == "new"){
+        finalObj["refs"][containername] = {
+            new: containertype,
+            store: {
+              where: storage
+            }
+        }
+      }
+      else{
+        finalObj["refs"][containername] = {
+          id: containerid,
+          store: {
+              where: storage
+          }
+        }
+      }
+    }
+    return JSON.stringify(finalObj, null, '\t')
+  }
 };
