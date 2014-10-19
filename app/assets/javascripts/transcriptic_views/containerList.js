b52a5323ceba73a14d37c0f8d5d0c7728005dffd
James Robinson <james.michael.robinson@googlemail.com>
2014-10-18 22:39:22 -0700

    Populate container drop-down with new containers

--- app/assets/javascripts/transcriptic_views/containerList.js
+++ app/assets/javascripts/transcriptic_views/containerList.js
@@ -1,13 +1,16 @@
 Transcriptic.ContainerList = function(containerListSelector) {
   this.$containerList = $(containerListSelector);
+  this.$containerDropdown = $("#containers_selector");
 };
 
 Transcriptic.ContainerList.prototype = {
   refresh: function() {
     this.$containerList.empty();
+    this.$containerDropdown.empty();
     for(var c in Transcriptic.organization.containers) {
       var container = Transcriptic.organization.containers[c];
       this.$containerList.append("<p>" + container.containerName + "</p>");
+      this.$containerDropdown.append("<option value='" + container.containerName + "'>" + container.containerName + "</option>");
     }
   }
 };
