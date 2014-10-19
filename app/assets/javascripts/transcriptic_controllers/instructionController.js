ed214be87b5c33eb8627fe7519b6965ea43245d4
James Robinson <james.michael.robinson@googlemail.com>
2014-10-18 21:33:47 -0700

    Make instructions clickable

--- app/assets/javascripts/transcriptic_controllers/instructionController.js
+++ app/assets/javascripts/transcriptic_controllers/instructionController.js
@@ -2,6 +2,7 @@ Transcriptic.InstructionController = function(instructionForm, instructionList) 
   this.instructionForm = instructionForm;
   this.instructionList = instructionList;
   this.instructionForm.bindEventListeners(this);
+  this.instructionList.bindEventListeners(this);
 };
 
 Transcriptic.InstructionController.prototype = {
@@ -9,5 +10,8 @@ Transcriptic.InstructionController.prototype = {
     var instruction = new Transcriptic.Instruction(instructionType);
     Transcriptic.organization.currentProject.currentRun.addInstruction(instruction);
     this.instructionList.refresh();
+  },
+  showInstruction: function(index) {
+    
   }
 };
--- app/assets/javascripts/transcriptic_views/instructionList.js
+++ app/assets/javascripts/transcriptic_views/instructionList.js
@@ -3,12 +3,21 @@ Transcriptic.InstructionList = function(instructionListSelector) {
 };
 
 Transcriptic.InstructionList.prototype = {
+  bindEventListeners: function(controller) {
+    this.controller = controller;
+    this.$instructionList.on("click", ".instruction", this.handleInstructionClick.bind(this));
+  },
+  handleInstructionClick: function(evt) {
+    evt.preventDefault();
+    var instructionIndex = $(evt.currentTarget).data("index");
+    this.controller.showInstruction(instructionIndex)
+  },
   refresh: function() {
     this.$instructionList.empty();
     var instructions = Transcriptic.organization.currentProject.currentRun.instructions;
     for(var c in instructions) {
       var instruction = instructions[c];
-      this.$instructionList.append("<p>" + instruction.instructionType + "</p>");
+      this.$instructionList.append("<p><a href='#' class='instruction' data-index='" + c + "'>" + instruction.instructionType + "</a></p>");
     }
   }
 };
