8dc9c1a2fb1b17816a99683d9256105e442aa323
Tali Herzka <tali@herzka.com>
2014-10-18 20:54:49 -0700

    made well have formatted reference

--- app/assets/javascripts/transcriptic_models/well.js
+++ app/assets/javascripts/transcriptic_models/well.js
@@ -2,8 +2,9 @@ Transcriptic.Well = function(container, wellIndex, volume) {
   this.container = container;
   this.wellIndex = wellIndex;
   this.volume = volume;
+  this.reference = this.container+"/"+this.wellIndex
 };
 
 Transcriptic.Well.prototype = {
-  
+  	
 };
