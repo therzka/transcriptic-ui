896cc44ef6cbe8e205ea8191b1ef3fabda9bb9fd
Tali Herzka <tali@herzka.com>
2014-10-18 23:54:10 -0700

    added instructions for mix and consolidate

---
+++ app/assets/javascripts/transcriptic_models/instructions/mix.js
@@ -0,0 +1,18 @@
+Transcriptic.Instruction.Mix = function(mixData) {
+  this.well = mixData.well
+  this.speed = mixData.speed
+  this.volume = mixData.volume
+  this.repititions = mixData.repititions
+};
+  
+Transcriptic.Instruction.Mix.prototype = {
+  addTo: function(well) {
+    this.to.push(well);
+  },
+
+  encodeAction: function() {
+    var mixes = [];
+    mixes.push({well: this.well.reference(), volume: this.volume, speed: this.speed, repititions:this.repititions,})
+    return {mix: mixes};
+  }
+};
