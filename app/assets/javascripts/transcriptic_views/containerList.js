deebf5da186733839c067cee31d421aa5e49ae25
James Robinson <james.michael.robinson@googlemail.com>
2014-10-18 18:29:51 -0700

    Refresh container list on creating new container

--- app/assets/javascripts/transcriptic_views/containerList.js
+++ app/assets/javascripts/transcriptic_views/containerList.js
@@ -3,5 +3,11 @@ Transcriptic.ContainerList = function(containerListSelector) {
 };
 
 Transcriptic.ContainerList.prototype = {
-  
+  refresh: function() {
+    this.$containerList.empty();
+    for(var c in Transcriptic.organization.containers) {
+      var container = Transcriptic.organization.containers[c];
+      this.$containerList.append("<p>" + container.containerName + "</p>");
+    }
+  }
 };
