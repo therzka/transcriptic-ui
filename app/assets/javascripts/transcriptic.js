8eb537a0d73e3389717841dfcadfd0cbfb86967b
James Robinson <james.michael.robinson@googlemail.com>
2014-10-20 01:27:32 -0700

    Cleanup old container form widget JS

--- app/assets/javascripts/transcriptic.js
+++ app/assets/javascripts/transcriptic.js
@@ -3,7 +3,6 @@ Transcriptic.Instruction = {};
 
 $(document).ready(function() {
   new Transcriptic.OrganizationInitializer();
-  new Transcriptic.ContainerFormWidget();
   new Transcriptic.ContainerList.ContainerListWidget("#container_list");
   new Transcriptic.InstructionFormWidget();
   new Transcriptic.SubmitFormWidget();
