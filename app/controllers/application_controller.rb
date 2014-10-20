8567932c346f674ac89a04ccaca80deb63df6bb0
Elena Sharma <elenasharma3@gmail.com>
2014-10-19 20:04:49 -0700

    Remove lab id from signup, all devise files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:lab_id, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:lab_id, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
   end
 
   def authenticate_user!
--- db/migrate/20141018075844_add_lab_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLabIdToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :lab_id, :string, null: false
-  end
-end
