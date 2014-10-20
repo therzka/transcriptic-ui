bf4fea51475d84356b0e9b39e5983ed61d521014
James Robinson <james.michael.robinson@googlemail.com>
2014-10-19 20:01:50 -0700

    Update test data generation

--- app/assets/javascripts/transcriptic_views/testData.js
+++ app/assets/javascripts/transcriptic_views/testData.js
@@ -1,16 +1,16 @@
 TestData = {
 	generateTestContainer: function(evt) {
+		var containerID = Math.floor(Math.random() * 10);
 		var containerData = {
-		  containerName:  "hello" + Math.floor(Math.random() * 10),
-		  containerID:    "1",
+		  containerName:  "container" + containerID,
+		  containerID:    containerID.toString(),
 		  containerType:  "96-pcr",
 		  storage:        "ambient"
 		}
 		Transcriptic.organization.addContainer(new Transcriptic.Container(containerData));
 		$("#container_list").append("<p>" + containerData.containerName + "</p>");
 	},
-
-	generateTestInstruction: function(evt) {
+	generateTransferInstruction: function(evt) {
 		var fromContainer = TestData.getRandomContainer();
 		var toContainer = TestData.getRandomContainer();
 		
@@ -28,15 +28,34 @@ TestData = {
 
 		var instruction = new Transcriptic.Instruction.Transfer(instructionData);
 		Transcriptic.organization.currentProject.currentRun.addInstruction(instruction);
-		// $("#instruction_list").append("Pipette(distribute) 100ml from blah/2 to blah/4")
+		var instructions = Transcriptic.organization.currentProject.currentRun.instructions;
+		$("#instruction_list").append(instruction.toString());
+	},
+	generateDistributeInstruction: function(evt) {
+		var fromContainer = TestData.getRandomContainer();
+		var toContainer = TestData.getRandomContainer();
+		
+		var fromWell = TestData.getRandomWell(fromContainer);
+		var toWell = TestData.getRandomWell(toContainer);
+
+		var volume = Math.floor(Math.random() * 1000);
+
+		var instructionData = {
+			container: fromContainer,
+			from: fromWell,
+			to: [toWell],
+			volume: volume
+		}
+
+		var instruction = new Transcriptic.Instruction.Distribute(instructionData);
+		Transcriptic.organization.currentProject.currentRun.addInstruction(instruction);
 		var instructions = Transcriptic.organization.currentProject.currentRun.instructions;
 		$("#instruction_list").append(instruction.toString());
 	},
 	bindEventListeners: function(){
-		var $testContainer = $("button#test_container");
-		var $testInstruction = $("button#test_instruction");
-		$testContainer.on("click", TestData.generateTestContainer.bind(this));
-		$testInstruction.on("click", TestData.generateTestInstruction.bind(this));
+		$("button#test_container").on("click", TestData.generateTestContainer.bind(this));
+		$("button#test_transfer").on("click", TestData.generateTransferInstruction.bind(this));
+		$("button#test_distribute").on("click", TestData.generateDistributeInstruction.bind(this));
 	},
 	getRandomContainer: function(){
 		var numContainers = Transcriptic.organization.containers.length;
