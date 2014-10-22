fb2a00e087271356c07a37ced5acada5bd64a12f
James Robinson <james.michael.robinson@googlemail.com>
2014-10-21 20:51:33 -0700

    Set instruction accordion to sortable with template callback

--- app/assets/javascripts/widgets/instructionList/instructionListView.js
+++ app/assets/javascripts/widgets/instructionList/instructionListView.js
@@ -1,6 +1,7 @@
 Transcriptic.InstructionList.InstructionListView = function(instructionListSelector) {
   this.$list = $(instructionListSelector);
   this.instructionTemplate = Handlebars.compile($("#instruction-template").html());
+  this.setSortable();
 };
 
 Transcriptic.InstructionList.InstructionListView.prototype = {
@@ -10,5 +11,13 @@ Transcriptic.InstructionList.InstructionListView.prototype = {
       this.$list.append(this.instructionTemplate(instructions[i]));
     }
     this.$list.find(".collapse").last().collapse('toggle');
+  },
+  setSortable: function() {
+    this.$list.sortable({
+      update: this.handleSortEvent.bind(this)
+    });
+  },
+  handleSortEvent: function(evt) {
+    
   }
 };
